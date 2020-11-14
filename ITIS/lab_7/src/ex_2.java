c7e79ce55060e08659ffdcf7c6a62dfe40436e36
Васильев Андрей <vasilyev@tochka.com>
2020-11-14 12:37:26 +0500

    lab 7: 1,2

--- ITIS/lab_7/src/ex_2.java
+++ ITIS/lab_7/src/ex_2.java
@@ -1,44 +1,45 @@
 /**
- * 2. Напишите программу, в которой есть суперкласс с приватным текстовым полем.
+ * 2. + Напишите программу, в которой есть суперкласс с приватным текстовым полем.
  *
- * 1) В базовом классе должен быть метод для присваивания значения полю: без параметров и с
+ * 1) + В базовом классе должен быть метод для присваивания значения полю: без параметров и с
  * одним текстовым параметром.
  *
- * 2) Объект суперкласса создается передачей одного текстового аргумента конструктору.
+ * 2) + Объект суперкласса создается передачей одного текстового аргумента конструктору.
  *
- * 3) Доступное только для чтения свойство результатом возвращает длину текстовой строки.
+ * 3) + Доступное только для чтения свойство результатом возвращает длину текстовой строки.
  * На основе суперкласса создается подкласс.
  *
- * 4) В подклассе появляется дополнительное открытое целочисленное поле.
+ * 4) + В подклассе появляется дополнительное открытое целочисленное поле.
  *
  * В классе должны быть такие версии метода для присваивания значений полям (используется переопределение
  * и перегрузка метода из суперкласса):
- * - без параметров,
- * - с текстовым параметром,
- * - с целочисленным параметром,
- * - с текстовым и целочисленным параметром.
+ * + без параметров,
+ * + с текстовым параметром,
+ * + с целочисленным параметром,
+ * + с текстовым и целочисленным параметром.
  *
- * - У конструктора подкласса два параметра (целочисленный и текстовый).
+ * + У конструктора подкласса два параметра (целочисленный и текстовый).
  */
 
 public class ex_2 {
 
     public static void main(String[] args) {
 
-        Sub_1 subCls = new Sub_1("subslass text");
+        Sub_2 subCls = new Sub_2(3, "superClass text");
 
-        //superText = null
-        //sbText = "subslass text"
+        //superText = "superClass text"
+        //num = 3
         System.out.println(subCls.toString());
 
-        //просто отделить вывод
-        System.out.println();
+        subCls.initFields("two");
+        //superText = "twoR"
+        //num = -1
+        System.out.println(subCls.toString());
 
-        Sub_1 subCls_2 = new Sub_1("super text 1", "subslass text");
-
-        //superText = "super text 1"
-        //sbText = "subslass text"
-        System.out.println(subCls_2.toString());
+        subCls.initFields(1, "one");
+        //superText = "one"
+        //num = 1
+        System.out.println(subCls.toString());
     }
 }
 
@@ -46,37 +47,53 @@ class Super_2 {
 
     private String superText;
 
-    Super_2() {
-        //значение текста = значение по умолчанию для экземпляров класса,
-        // для строки = null
-    }
+    final int textLength = superText != null ? superText.length() : 0;
 
-    Super_2(String superText) {
-        this.superText = superText;
-    }
+    Super_2() { initText(); }
+    Super_2(String superText) { this.superText = superText; }
+
+    void initText() { superText = "default"; }
+    void initText(String text) { superText = text; }
 
     @Override
     public String toString() {
-        return "Class: Super_1, superText: " + superText;
+        return "Class: Super_2, superText: " + superText;
     }
 }
 
-class Sub_2 extends Super_1 {
+class Sub_2 extends Super_2 {
 
-    private final String subText;
+    int num;
 
-    Sub_2(String subText) {
-        super();
-        this.subText = subText;
+    Sub_2(int num, String text) {
+        this.num = num;
+        super.initText(text);
     }
 
-    Sub_2(String superText, String subText) {
-        super(superText);
-        this.subText = subText;
+    private void initNumDefault() { num = -1; }
+
+    void initFields() {
+        initNumDefault();
+        super.initText();
+    }
+
+    void initFields(int num) {
+        this.num = num;
+        super.initText();
+    }
+
+    void initFields(String text) {
+        initNumDefault();
+        super.initText(text);
+    }
+
+    void initFields(int num, String text) {
+        this.num = num;
+        super.initText(text);
     }
 
     @Override
     public String toString() {
-        return super.toString() + "\nClass: " + this.getClass().getName() + ", subText: " + subText;
+        return super.toString() + "\nClass: " + this.getClass().getName() + ", num: " + num;
     }
 }
