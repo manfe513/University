7315c5fdc932352bb57a9d47b3fadba856c36ab4
Васильев Андрей <vasilyev@tochka.com>
2021-05-09 21:31:15 +0500

    fb init async

--- 2 semester/Chatico/chatico/lib/main.dart
+++ 2 semester/Chatico/chatico/lib/main.dart
@@ -1,13 +1,38 @@
 import 'package:chatico/LaunchScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) => MaterialApp(
         title: "Chatico",
-        home: LaunchScreen(),
         theme: ThemeData(primaryColor: Colors.blue),
+        home: FutureBuilder(
+            future: _fbApp,
+            builder: (context, snapshot) {
+
+              if(snapshot.hasError) {
+                print("Firebase init error: ${snapshot.error.toString()}");
+                return Text("Firebase error");
+
+              } else if (snapshot.hasData) {
+                return LaunchScreen();
+
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            }
+        )
       );
 }
