97250f83beeb0d5e7d5f974f0b0d1958470b2366
Васильев Андрей <vasilyev@tochka.com>
2021-05-09 15:10:43 +0500

    Chatico: Messages list debugging

--- 2 semester/Chatico/chatico/lib/chat/ChatMessage.dart
+++ 2 semester/Chatico/chatico/lib/chat/ChatMessage.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 
 class ChatMessage extends StatelessWidget {
-  const ChatMessage({Key key}) : super(key: key);
+
+  ChatMessage({this.text});
+
+  final String text;
+
+  String _name = "Your name";
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text(_name[0])),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(_name, style: Theme.of(context).textTheme.headline4),
+              Container(
+                margin: EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              ),
+            ],
+          ),
+        ],
+      )
+    );
   }
 }
--- 2 semester/Chatico/chatico/lib/chat/ChatScreen.dart
+++ 2 semester/Chatico/chatico/lib/chat/ChatScreen.dart
@@ -1,3 +1,4 @@
+import 'package:chatico/chat/ChatMessage.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -6,7 +7,9 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Chatico: chat")), body: ChatScreenBody());
+        appBar: AppBar(title: Text("Chatico: chat")),
+        body: ChatScreenBody()
+    );
   }
 }
 
@@ -16,36 +19,78 @@ class ChatScreenBody extends StatefulWidget {
 }
 
 class _ChatScreenBodyState extends State<ChatScreenBody> {
-  TextEditingController _textController = TextEditingController();
+
+  final List<ChatMessage> _messages = [];
+  final TextEditingController _textController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
 
   @override
-  Widget build(BuildContext context) => Row(children: [
+  Widget build(BuildContext context) =>
+      Column(
+        children: [
+          Flexible(
+              child: ListView.builder(
+                padding: EdgeInsets.all(8.0),
+                reverse: true,
+                itemBuilder: (_, int index) => _messages[index],
+                itemCount: _messages.length,
+              )
+          ),
+          Divider(height: 1.0),
+          Container(
+              decoration: BoxDecoration(
+                  color: Theme
+                      .of(context)
+                      .cardColor
+              ),
+              child: _buildTextComposer()
+          )
+        ],
+      );
 
-    Flexible(child: _buildTextComposer()),
-    IconTheme(
-      data: IconThemeData(color: Theme.of(context).accentColor),
-      child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 4.0),
-        child: IconButton(
-          icon: const Icon(Icons.send),
-          onPressed: () => _handleSubmitted(_textController.text),
-        ),
-      ),
-    )
-  ]);
+
+  //     Row(children: [
+  //
+  //   Flexible(child: _buildTextComposer()),
+
+  // ]);
 
   Widget _buildTextComposer() {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 8.0),
-      child: TextField(
-        controller: _textController,
-        onSubmitted: _handleSubmitted,
-        decoration: InputDecoration.collapsed(hintText: 'Send a message'),
-      ),
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin:
+        EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            Flexible(
+                child: TextField(
+                  controller: _textController,
+                  onSubmitted: _handleSubmitted,
+                  focusNode: _focusNode,
+                  decoration: InputDecoration.collapsed(hintText: 'Send a message'),
+                )
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: const Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text),
+              ),
+            )
+          ],
+        ),
+      )
     );
   }
 
   void _handleSubmitted(String text) {
+    ChatMessage newMsg = ChatMessage(text: text);
+
+    setState(() {
+      _messages.insert(0, newMsg);
+    });
+    _focusNode.requestFocus();
     _textController.clear();
   }
 }
