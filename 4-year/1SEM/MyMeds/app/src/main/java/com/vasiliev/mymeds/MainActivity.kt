c6ca2538ecffc9cc4026c7a5f05aca60519cf47c
Васильев Андрей <vasilyev@tochka.com>
2023-12-27 22:01:16 +0500

    4yr/1sem/myMeds 3

--- 4-year/1SEM/MyMeds/app/src/main/java/com/vasiliev/mymeds/MainActivity.kt
+++ 4-year/1SEM/MyMeds/app/src/main/java/com/vasiliev/mymeds/MainActivity.kt
@@ -8,8 +8,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
@@ -19,9 +24,11 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.vasiliev.mymeds.ui.theme.MyMedsTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,21 +38,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyMedsTheme {
-                Scaffold(
-                    topBar = {
-                        TopAppBar(
-                            colors = TopAppBarDefaults.topAppBarColors(
-                                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                                titleContentColor = MaterialTheme.colorScheme.primary,
-                            ),
-                            title = { Text(text = "MyMeds") }
-                        )
-                    }
-                ) { contentPadding ->
-                    Text(text = "asdfasd", modifier = Modifier.padding(top = contentPadding.calculateTopPadding()))
-//                    Main("Android")
-                }
-
+                Main()
             }
         }
     }
@@ -60,24 +53,56 @@ fun Drawer() {
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Main(name: String, modifier: Modifier = Modifier) {
-    Column {
-        TextField(
-            value = "",
-            onValueChange = { },
-            label = { Text("Дата начала") }
-        )
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Авторизация")
+fun Main() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = { Text(text = "MyMeds") }
+            )
         }
+    ) { contentPadding ->
+        Box(
+            modifier = Modifier.fillMaxSize().padding(16.dp)
+        ) {
+            Column {
+                Text(
+                    text = "asdfasd",
+                    modifier = Modifier.padding(top = contentPadding.calculateTopPadding())
+                )
+
+            }
+            FloatingActionButton(
+                onClick = { /*TODO*/ },
+                Modifier.align(alignment = Alignment.BottomEnd)
+            ) {
+                Icon(Icons.Filled.Add, "Floating action button.")
+            }
+        }
+
     }
+
+//    Column {
+//        TextField(
+//            value = "",
+//            onValueChange = { },
+//            label = { Text("Дата начала") }
+//        )
+//        Button(onClick = { /*TODO*/ }) {
+//            Text(text = "Авторизация")
+//        }
+//    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyMedsTheme {
-        Main("Android")
+        Main()
     }
 }
