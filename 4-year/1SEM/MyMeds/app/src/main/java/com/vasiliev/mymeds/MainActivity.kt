6259efa86b817633f3807374bde35470d09b77cf
Васильев Андрей <vasilyev@tochka.com>
2023-12-27 21:50:59 +0500

    4yr/1sem/myMeds 2

--- 4-year/1SEM/MyMeds/app/src/main/java/com/vasiliev/mymeds/MainActivity.kt
+++ 4-year/1SEM/MyMeds/app/src/main/java/com/vasiliev/mymeds/MainActivity.kt
@@ -3,42 +3,81 @@ package com.vasiliev.mymeds
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.vasiliev.mymeds.ui.theme.MyMedsTheme
 
 class MainActivity : ComponentActivity() {
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyMedsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                titleContentColor = MaterialTheme.colorScheme.primary,
+                            ),
+                            title = { Text(text = "MyMeds") }
+                        )
+                    }
+                ) { contentPadding ->
+                    Text(text = "asdfasd", modifier = Modifier.padding(top = contentPadding.calculateTopPadding()))
+//                    Main("Android")
                 }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Drawer() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Blue)
     )
+}
+
+@Composable
+fun Main(name: String, modifier: Modifier = Modifier) {
+    Column {
+        TextField(
+            value = "",
+            onValueChange = { },
+            label = { Text("Дата начала") }
+        )
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Авторизация")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyMedsTheme {
-        Greeting("Android")
+        Main("Android")
     }
 }
